abe274ee2823ffbb1737b0231d88a39c9e58834c
KelsCree <kelseycreehan@gmail.com>
2020-12-03 16:02:27 -0700

    Starter Redux app.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react'
 import CharactersContainer from './components/CharactersContainer';
+import { connect } from 'react-redux'
 
 class App extends Component{
 
@@ -12,7 +13,7 @@ class App extends Component{
   componentDidMount(){
     fetch("https://rickandmortyapi.com/api/character/")
     .then(response => response.json())
-    .then(({results}) => this.setState({characters: results}))
+    .then(({results}) => this.props.setCharacters(results))
   }
 
   render(){
@@ -24,4 +25,10 @@ class App extends Component{
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setCharacters: (characters) => dispatch({type: 'SET_CHARACTERS', payload: characters})
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App);
